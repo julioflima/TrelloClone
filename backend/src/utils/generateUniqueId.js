bae39a308cd4e6a74ab5917911496e3fffcdd79c
juloko <julio_flima@hotmail.com  git config --global user.name Your>
2020-08-14 07:00:07 -0300

    backeend with SQLite database DONE!

--- backend/src/utils/generateUniqueId.js
+++ backend/src/utils/generateUniqueId.js
@@ -1,5 +1,5 @@
 const crypto = require('crypto');
 
-module.exports =  function generateUniqueId() {
-    return crypto.randomBytes(4).toString('HEX');
+module.exports =  function generateUniqueId(bytes) {
+    return crypto.randomBytes(bytes).toString('HEX');
 }
